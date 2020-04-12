a5f3083abc8325a3173115422137b582df72b72d
Querrin <xdquerrin@gmail.com>
2020-04-12 09:24:05 +0200

    Initial commit

---
+++ src/main/java/com/hooklite/endshop/endshop/EndShop.java
@@ -0,0 +1,17 @@
+package com.hooklite.endshop.endshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EndShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
