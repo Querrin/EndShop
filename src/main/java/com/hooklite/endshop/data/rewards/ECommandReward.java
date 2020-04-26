b2b319f8419f6c7fad0a23cb812f4d6f62a769dc
Querrin <xdquerrin@gmail.com>
2020-04-26 12:45:35 +0200

    Implemented command reward

--- src/main/java/com/hooklite/endshop/data/rewards/ECommandReward.java
+++ src/main/java/com/hooklite/endshop/data/rewards/ECommandReward.java
@@ -1,5 +1,8 @@
 package com.hooklite.endshop.data.rewards;
 
+import com.hooklite.endshop.logging.MessageLogger;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class ECommandReward implements EReward {
@@ -7,7 +10,19 @@ public class ECommandReward implements EReward {
 
     @Override
     public void executeReward(Player player, RewardAction action, int amount) {
-        // TODO: Set the reward action
+        String command = reward;
+        command = command.replace("%player%", player.getName());
+
+        if (command.contains("(CONSOLE)")) {
+            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
+        } else if (command.contains("(PLAYER)")) {
+            Bukkit.getServer().dispatchCommand(player, reward);
+        } else {
+            // TODO: Better improperly configured command system (Possibly logging)
+            MessageLogger.toConsole(String.format("%sCommand executor of \"%s\" is improperly configured!", ChatColor.RED, command));
+            MessageLogger.toPlayer(player, String.format("%sCommand executor of \"%s\" is improperly configured!", ChatColor.RED, command));
+            MessageLogger.toPlayer(player, "Message the admins to fix the error and get your reward!");
+        }
     }
 
     @Override
