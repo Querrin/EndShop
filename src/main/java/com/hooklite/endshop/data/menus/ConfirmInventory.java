6d3ed93c963565ef664cb26dae5ce6201634091f
Querrin <xdquerrin@gmail.com>
2020-05-08 09:32:49 +0200

    Change inventory title to a more eye friendly one

--- src/main/java/com/hooklite/endshop/data/menus/ConfirmInventory.java
+++ src/main/java/com/hooklite/endshop/data/menus/ConfirmInventory.java
@@ -9,15 +9,16 @@ import org.bukkit.inventory.Inventory;
 
 public class ConfirmInventory {
     public Inventory getMenu(Item item) {
-        Inventory inventory = Bukkit.createInventory(new ConfirmMenu(), 45, String.format("%s%sCONFIRM", ChatColor.GREEN, ChatColor.BOLD));
+        Inventory inventory = Bukkit.createInventory(new ConfirmMenu(), 45, String.format("%s%sConfirm", ChatColor.DARK_GRAY, ChatColor.BOLD));
 
-        inventory.setItem(20, item.displayItem);
+        inventory.setItem(22, item.displayItem);
 
         inventory.setItem(10, MenuItemFactory.CANCEL_ITEM);
         inventory.setItem(11, MenuItemFactory.CANCEL_ITEM);
         inventory.setItem(12, MenuItemFactory.CANCEL_ITEM);
-        inventory.setItem(18, MenuItemFactory.CANCEL_ITEM);
         inventory.setItem(19, MenuItemFactory.CANCEL_ITEM);
+        inventory.setItem(20, MenuItemFactory.CANCEL_ITEM);
+        inventory.setItem(21, MenuItemFactory.CANCEL_ITEM);
         inventory.setItem(28, MenuItemFactory.CANCEL_ITEM);
         inventory.setItem(29, MenuItemFactory.CANCEL_ITEM);
         inventory.setItem(30, MenuItemFactory.CANCEL_ITEM);
@@ -25,9 +26,9 @@ public class ConfirmInventory {
         inventory.setItem(14, MenuItemFactory.CONFIRM_ITEM);
         inventory.setItem(15, MenuItemFactory.CONFIRM_ITEM);
         inventory.setItem(16, MenuItemFactory.CONFIRM_ITEM);
-        inventory.setItem(21, MenuItemFactory.CONFIRM_ITEM);
-        inventory.setItem(22, MenuItemFactory.CONFIRM_ITEM);
         inventory.setItem(23, MenuItemFactory.CONFIRM_ITEM);
+        inventory.setItem(24, MenuItemFactory.CONFIRM_ITEM);
+        inventory.setItem(25, MenuItemFactory.CONFIRM_ITEM);
         inventory.setItem(32, MenuItemFactory.CONFIRM_ITEM);
         inventory.setItem(33, MenuItemFactory.CONFIRM_ITEM);
         inventory.setItem(34, MenuItemFactory.CONFIRM_ITEM);
