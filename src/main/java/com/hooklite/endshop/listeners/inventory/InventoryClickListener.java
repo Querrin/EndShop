77705cbbad19cc0d9b8528538b92ed54e21d6c6e
Querrin <xdquerrin@gmail.com>
2020-05-11 17:03:16 +0200

    Fix EventException where if player would click in a furnace the plugin would output errors

--- src/main/java/com/hooklite/endshop/listeners/inventory/InventoryClickListener.java
+++ src/main/java/com/hooklite/endshop/listeners/inventory/InventoryClickListener.java
@@ -18,6 +18,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
@@ -25,7 +26,7 @@ import org.bukkit.persistence.PersistentDataType;
 public class InventoryClickListener implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if(event.getClickedInventory() != null && event.getCurrentItem() != null) {
+        if(event.getClickedInventory() != null && event.getCurrentItem() != null && event.getClickedInventory().getType() == InventoryType.CHEST) {
             int clickedSlot = event.getSlot();
             Player player = (Player) event.getWhoClicked();
             Inventory clickedInventory = event.getInventory();
