a76ba217b5373dba9e336415db5adfc24e2472ad
Querrin <xdquerrin@gmail.com>
2020-05-23 12:55:40 +0200

    Implemented bStats

--- src/main/java/com/hooklite/endshop/EndShop.java
+++ src/main/java/com/hooklite/endshop/EndShop.java
@@ -4,14 +4,18 @@ import com.hooklite.endshop.commands.CommandManager;
 import com.hooklite.endshop.config.Configuration;
 import com.hooklite.endshop.listeners.TransactionListener;
 import com.hooklite.endshop.listeners.InventoryClickListener;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import static org.bukkit.Bukkit.getPluginManager;
 
-
 public final class EndShop extends JavaPlugin {
+    private final int BSTATS_PLUGIN_ID = 7622;
+
     @Override
     public void onEnable() {
+        new Metrics(this, BSTATS_PLUGIN_ID);
+
         getPluginManager().registerEvents(new InventoryClickListener(), this);
         getPluginManager().registerEvents(new TransactionListener(), this);
         Configuration.configurePlugin(this);
